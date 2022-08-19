ff045e314c4353b25aa08a5b8ce74be102121020
anaflgg <anaflaviananias3@gmail.com>
2022-08-19 17:55:25 -0300

    arquivos do projeto

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+const ButtonPrev = document.querySelector('.btn-prev');
+const ButtonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    if (APIResponse.status == 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
+}
+
+const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Loading ...';
+
+    const data = await fetchPokemon(pokemon); 
+    console.log(data);
+    if (data) {
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = '';
+        searchPokemon = data.id;
+    } else {
+        pokemonImage.style.display = 'none';
+        pokemonName.innerHTML = 'Not found :c';
+        pokemonNumber.innerHTML = '';
+    }
+
+}
+//buscar api
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+
+    renderPokemon(input.value.toLowerCase());
+});
+
+ButtonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+});
+
+ButtonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+    
+
+renderPokemon('searchPokemon');
